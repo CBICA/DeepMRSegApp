d241aaa8e86271301fddf748bb9d6491809f75e4
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-03-30 14:47:33 -0400

    cleanup wrapper script

--- Modules/CbicaDeepMRSeg/resources/deepmrsegwrapper.py
+++ Modules/CbicaDeepMRSeg/resources/deepmrsegwrapper.py
@@ -5,83 +5,52 @@ Created on Wed Mar 24 19:16:24 2021
 @author: SinghAs
 """
 
+#assumes DeepMRSeg is installed 
 from DeepMRSeg import deepmrseg_test
+
 import SimpleITK as sitk
 import sys as _sys
 import os
   
-#if len ( sys.argv ) < 3:
-#    print( "Usage: Image Intensity Invert <input> <output>" )
-#    sys.exit ( 1 )
-  
-# reader = sitk.ImageFileReader()
-# reader.SetFileName ( sys.argv[1] )
-# image = reader.Execute()
+print("wrapper called") # info for mitk debugging
 
-# inverter = sitk.InvertIntensityImageFilter()
-# output = inverter.Execute(image)
-  
-# writer = sitk.ImageFileWriter()
-# writer.SetFileName ( sys.argv[2] )
-# writer.Execute ( output );
-print("wrapper called")
+# function to write input data_list as expected by DeepMRSeg
 def csvwriter(fname, data_list):
 	import csv
 	with open(fname, 'w', newline='') as file:
 		writer = csv.writer(file, delimiter=',')
 		writer.writerows(data_list)
     
-#define a temp location to read/write
+#pre-defined temp location to read/write
 predefinedTempPath = 'C:/workspace/projects/DeepMRSegApp/Modules/CbicaDeepMRSeg/resources/temp/'
 
-#save the incoming simple itk image to a pre-defined location
+#save the incoming simple itk image to pre-defined location
 inputFileSavePath = predefinedTempPath + 'inputFile.nii.gz'
 writer = sitk.ImageFileWriter()
 writer.SetFileName (inputFileSavePath )
 writer.Execute ( in_image );
 print("incoming simple itk image saved")
 
-#deepmrseg specific stuff
-#_sys.argv = ["deeprmseg_test",
-#	      "--sList",
-#	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Lists\\Testing_n10_FileList.txt",
-#	      "--idCol",
-#	      "ID",
-#	      "--refMod",
-#	      "T1",
-#	      "--outCol",
-#	      "out",
-#	      "--roi",
-#	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Lists\\SkullStripping_ROI_Indices.csv",
-#	      "--mdlDir",
-#	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Models\\bestmodels\\",
-#	      "--prep",
-#	      "--num_classes",
-#	      "2",
-#	      "--rescale",
-#	      "norm",
-#	      "--xy_width",
-#	      "256",
-#	      "--ressize",
-#	      "1",
-#	      "--reorient",
-#	      "LPS",
-#	      "--verb",
-#	      "--nJobs",
-#	      "1"]
+#deepmrseg specific stuff starts
 
+#path where output from DeepMRSeg will be saved
 outPath = predefinedTempPath + "../Out/test_1_label.nii.gz"
+
+#create data list for 1st data--> this needs to happen dynamically
 data_list = [["ID", "T1", "out"],
              ["test_1", inputFileSavePath, outPath]]
 
+#path to save csv file
 csvpath = predefinedTempPath + "\Testing_n10_FileList.txt"
 
+#write csv file
 csvwriter(csvpath,data_list)
 print("csv saved")
 
+#create args for passing to DeepMRSeg
 _sys.argv = ["deeprmseg_test",
 	      "--sList",
-	      csvpath,
+	      csvpath, #this is what we created above
 	      "--idCol",
 	      "ID",
 	      "--refMod",
@@ -107,16 +76,14 @@ _sys.argv = ["deeprmseg_test",
 	      "--nJobs",
 	      "1"]
 
+#call DeepMRSeg
 deepmrseg_test._main()
 
 print("deepmrseg run")
+#deepmrseg specific stuff ends here
 
 #read the output image as simple itk image from pre-defined location
 reader = sitk.ImageFileReader()
-#outputPath = predefinedTempPath + 'test_1_label.nii.gz'
 reader.SetFileName ( outPath )
 out_image = reader.Execute()
 print("result read as simple itk image")
-#
-#inverter = sitk.InvertIntensityImageFilter()
-#out_image = inverter.Execute(in_image)

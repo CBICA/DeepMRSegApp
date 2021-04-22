51029ec0d98f12aac5359fffcd8d4238dc067642
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-04-22 16:44:37 -0400

    cleanup

--- Modules/ModuleList.cmake
+++ Modules/ModuleList.cmake
@@ -1,5 +1,5 @@
-### Change the prefix that gets added automatically to CaPTk
-# set(MITK_MODULE_NAME_PREFIX "CaPTk")
+### Change the prefix that gets added automatically to DeepMRSegApp
+# set(MITK_MODULE_NAME_PREFIX "DeepMRSegApp")
 
 ### Attention: If a module depends on another, it should be after it!
 

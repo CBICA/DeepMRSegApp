06d1c01c50e88fdeb089abf719136768a8c54589
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-05-17 17:35:45 -0400

    integrating predictClasses function into wrapper..wip

--- Modules/CbicaDeepMRSeg/resources/deepmrsegwrapper.py
+++ Modules/CbicaDeepMRSeg/resources/deepmrsegwrapper.py
@@ -12,9 +12,12 @@ from DeepMRSeg import deepmrseg_test
 
 import SimpleITK as sitk
 import sys as _sys
-import os
+import os as _os
+import nibabel
+import sitk2nibabel
   
 print("wrapper called") # info for mitk debugging
+_sys.stdout.flush()
 
 # function to write input data_list as expected by DeepMRSeg
 def csvwriter(fname, data_list):
@@ -26,12 +29,25 @@ def csvwriter(fname, data_list):
 #pre-defined temp location to read/write
 predefinedTempPath = 'C:/workspace/projects/DeepMRSegApp/Modules/CbicaDeepMRSeg/resources/temp/'
 
+mdlPath = "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Models\\bestmodels\\"
+allmodels = []
+#for mDir in mdlPath:
+for checkpoint in _os.listdir( mdlPath ):
+    cppath = _os.path.join( mdlPath + '\\' + checkpoint )
+    print('cppath',cppath)
+    deepmrseg_test.load_model( allmodels,cppath )
+
+#_sys.exit(0)
+#convert sitk to nibabel image
+nibObj = sitk2nibabel.SimpleITKAsNibabel(in_image)
+print('nibObj',nibObj)
+
 #save the incoming simple itk image to pre-defined location
-inputFileSavePath = predefinedTempPath + 'inputFile.nii.gz'
-writer = sitk.ImageFileWriter()
-writer.SetFileName (inputFileSavePath )
-writer.Execute ( in_image );
-print("incoming simple itk image saved")
+#inputFileSavePath = predefinedTempPath + 'inputFile.nii.gz'
+#writer = sitk.ImageFileWriter()
+#writer.SetFileName (inputFileSavePath )
+#writer.Execute ( in_image );
+#print("incoming simple itk image saved")
 
 #deepmrseg specific stuff starts
 
@@ -39,47 +55,64 @@ print("incoming simple itk image saved")
 outPath = predefinedTempPath + "../Out/test_1_label.nii.gz"
 
 #create data list for 1st data--> this needs to happen dynamically
-data_list = [["ID", "T1", "out"],
-             ["test_1", inputFileSavePath, outPath]]
+#data_list = [["ID", "T1", "out"],
+#             ["test_1", inputFileSavePath, outPath]]
 
-#path to save csv file
-csvpath = predefinedTempPath + "\Testing_n10_FileList.txt"
+##path to save csv file
+#csvpath = predefinedTempPath + "\Testing_n10_FileList.txt"
 
-#write csv file
-csvwriter(csvpath,data_list)
-print("csv saved")
+##write csv file
+#csvwriter(csvpath,data_list)
+#print("csv saved")
 
-#create args for passing to DeepMRSeg
-_sys.argv = ["deeprmseg_test",
-	      "--sList",
-	      csvpath, #this is what we created above
-	      "--idCol",
-	      "ID",
-	      "--refMod",
-	      "T1",
-	      "--outCol",
-	      "out",
-	      "--roi",
-	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Lists\\SkullStripping_ROI_Indices.csv",
-	      "--mdlDir",
-	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Models\\bestmodels\\",
-	      "--prep",
-	      "--num_classes",
-	      "2",
-	      "--rescale",
-	      "norm",
-	      "--xy_width",
-	      "256",
-	      "--ressize",
-	      "1",
-	      "--reorient",
-	      "LPS",
-	      "--verb",
-	      "--nJobs",
-	      "1"]
+##create args for passing to DeepMRSeg
+#_sys.argv = ["deeprmseg_test",
+#	      "--sList",
+#	      csvpath, #this is what we created above
+#	      "--idCol",
+#	      "ID",
+#	      "--refMod",
+#	      "T1",
+#	      "--outCol",
+#	      "out",
+#	      "--roi",
+#	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Lists\\SkullStripping_ROI_Indices.csv",
+#	      "--mdlDir",
+#	      "C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Models\\bestmodels\\",
+#	      "--prep",
+#	      "--num_classes",
+#	      "2",
+#	      "--rescale",
+#	      "norm",
+#	      "--xy_width",
+#	      "256",
+#	      "--ressize",
+#	      "1",
+#	      "--reorient",
+#	      "LPS",
+#	      "--verb",
+#	      "--nJobs",
+#	      "1"]
 
 #call DeepMRSeg
-deepmrseg_test._main()
+#deepmrseg_test._main()
+
+print('calling predictClasses')
+#call predecitClasses
+resultnibobj = deepmrseg_test.predictClasses( refImg=nibObj,
+    otherImg=None,
+    num_classes=2,
+    allmodels=allmodels,          ### This is loaded in main()
+    roi_indices="C:\\workspace\\projects\\DeepMRSegApp\\Modules\\CbicaDeepMRSeg\\resources\\Lists\\SkullStripping_ROI_Indices.csv",       ### Path to csv
+    out=None,
+    probs=False,
+    rescalemethod='batch',
+    ressize=float(1),
+    orient='LPS',
+    xy_width=256,
+    batch_size=64,
+    nJobs=1 )
+
 
 print("deepmrseg run")
 #deepmrseg specific stuff ends here
